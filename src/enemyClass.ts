f4f5e359cfe60b6870f5c61e73dab16bf3635b08
Nicolai20HTXR <skole.nicolai.wiese@gmail.com>
2023-03-17 11:40:36 +0100

    Constructor Enemy

--- src/enemyClass.ts
+++ src/enemyClass.ts
@@ -1,1 +1,11 @@
 //Nothing in here
+
+class Enemy {
+    pos: Point;
+    path: Array<Point>;
+    constructor(pos: Point, path: Array<Point>) {
+        this.pos = { x: pos.x, y: pos.y };
+        this.path = path;
+
+    }
+}
