8f21cde1101b95d07736f54578e2494b2cda66c6
SpicyGengu <lovro.antic1@gmail.com>
2023-03-27 11:21:52 +0200

    Create sprayTower.ts

---
+++ src/sprayTower.ts
@@ -0,0 +1,44 @@
+class SprayTower {
+  public shootTimeOut: number = 0;
+  public sprayTimeOut: number = 0;
+
+  constructor(
+    public size: number,
+    public positionX: number,
+    public positionY: number,
+    public rateOfFire: number,
+    public sprayTime: number
+  ) { }
+
+  update() {
+    this.shoot();
+    circle(this.positionX, this.positionY, this.size);
+  }
+
+  hitBoxHitCheck() {
+    if (dist(this.positionX, this.positionY, mouseX, mouseY) <= this.size * 2.5 / 2) {
+      print('hit');
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  shoot() {
+    if (this.shootTimeOut === this.rateOfFire) {
+      if (this.sprayTimeOut != this.sprayTime) {
+        push();
+        fill(0, 200, 0);
+        circle(this.positionX, this.positionY, this.size * 2.5);
+        pop();
+        this.hitBoxHitCheck();
+        this.sprayTimeOut++;
+      } else {
+        this.sprayTimeOut = 0;
+        this.shootTimeOut = 0;
+      }
+    } else {
+      this.shootTimeOut++;
+    }
+  }
+}
