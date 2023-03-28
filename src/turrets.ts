1b288e03e144d1c31b185cfca3abff06ac2a312e
Nicolai20HTXR <skole.nicolai.wiese@gmail.com>
2023-03-28 15:12:23 +0200

    rename bad naming

--- src/turrets.ts
+++ src/turrets.ts
@@ -78,30 +78,30 @@ function checkCache(marker: Marker): Marker {
 export function syncTurretObj() {
 	const markerTurrets = getTurrets();
 
-	markerTurrets.forEach((x) => {
-		if (!activeTurrets.has(x.id)) {
-			switch (x.turretType) {
+	markerTurrets.forEach((markerTurret) => {
+		if (!activeTurrets.has(markerTurret.id)) {
+			switch (markerTurret.turretType) {
 				case "gatling": {
-					activeTurrets.set(x.id, new GatlingTower(x.diameter, x.center.y, x.center.y, 25, 25, 0));
+					activeTurrets.set(markerTurret.id, new GatlingTower(markerTurret.diameter, markerTurret.center.y, markerTurret.center.y, 25, 25, 0));
 					break;
 				}
 				case "spray": {
-					activeTurrets.set(x.id, new SprayTower(x.diameter, x.center.y, x.center.y, 25, 0, 25));
+					activeTurrets.set(markerTurret.id, new SprayTower(markerTurret.diameter, markerTurret.center.y, markerTurret.center.y, 25, 0, 25));
 					break;
 
 				}
 			}
-		} else if (activeTurrets.has(x.id)) {
-			let tur = activeTurrets.get(x.id)!;
-			tur.diameter = x.diameter;
-			tur.angle = x.angle;
-			tur.positionX = x.center.x;
-			tur.positionY = x.center.y;
+		} else if (activeTurrets.has(markerTurret.id)) {
+			let tur = activeTurrets.get(markerTurret.id)!;
+			tur.diameter = markerTurret.diameter;
+			tur.angle = markerTurret.angle;
+			tur.positionX = markerTurret.center.x;
+			tur.positionY = markerTurret.center.y;
 		}
 	});
 }
 export function updateTurretObj() {
-	activeTurrets.forEach((x) => {
-		x.update(x.angle);
+	activeTurrets.forEach((markerTurret) => {
+		markerTurret.update(markerTurret.angle);
 	});
 }
