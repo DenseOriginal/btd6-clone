55f7c2a2d819ae271e09390dd10743e83dba2919
SpicyGengu <lovro.antic1@gmail.com>
2023-03-28 10:12:04 +0200

    gattling tower shit

---
+++ src/gatlingProjectileClass.ts
@@ -0,0 +1,40 @@
+export class GatlingProjectile {
+  public beenHit: number = 0;
+  public directionDegreeScaled: number;
+  public directionX: number;
+  public directionY: number;
+
+  constructor(
+    public size: number,
+    public speed: number,
+    public positionX: number,
+    public positionY: number,
+    public directionDegree: number,
+    ) {
+    this.directionDegreeScaled = directionDegree * 1000;
+    this.directionDegree = round(randomGaussian(this.directionDegreeScaled, 180)) / 1000;
+    this.directionX = cos(this.directionDegree);
+    this.directionY = sin(this.directionDegree);
+  }
+
+  update() {
+    this.hitBoxHitCheck()
+    if(this.beenHit < 1) {
+      this.positionX += this.directionX * this.speed;
+      this.positionY += this.directionY * this.speed;
+      circle(this.positionX, this.positionY, this.size);
+    }
+  }
+
+  hitBoxHitCheck() {
+    if(
+    //sæt en anden criteria plz
+    dist(this.positionX, this.positionY, mouseX, mouseY) < this.size / 2 
+    ) {
+      this.beenHit++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
---
+++ src/gatlingTowerClass.ts
@@ -0,0 +1,49 @@
+import { GatlingProjectile } from "./gatlingProjectileClass";
+
+class GatlingTower {
+  public barrelTipX: number = 0;
+  public barrelTipY: number = 0;  
+  public shots: GatlingProjectile[] = [];
+  public shootTimeOut: number = 0;
+  public angle: number = 0;
+
+  constructor(
+    public size: number,
+    public positionX: number,
+    public positionY: number,
+    public rateOfFire: number,
+    public projectileSpeed: number
+  ) { }
+
+  update(directionDegree) {
+    this.angle = directionDegree
+    const directionX = cos(this.angle);
+    const directionY = sin(this.angle);
+    this.barrelTipX = this.size * 1.5 * directionX + this.positionX;
+    this.barrelTipY = this.size * 1.5 * directionY + this.positionY;
+    line(this.positionX, this.positionY, this.barrelTipX, this.barrelTipY);
+    circle(this.positionX, this.positionY, this.size);
+    this.shoot();
+  }
+  
+  shoot() {
+    if(this.shootTimeOut === this.rateOfFire) {
+      this.shots.push(new GatlingProjectile(this.size * 0.5, this.projectileSpeed, this.barrelTipX, this.barrelTipY, this.angle));
+      this.shootTimeOut = 0;
+    } else {
+      this.shootTimeOut++;
+    }
+  }
+
+  updateShots() {
+    for (let i = this.shots.length - 1; i > 0; i--) {
+      this.shots[i].update();
+      if (this.shots[i].positionX > width
+        || this.shots[i].positionX < 0
+        || this.shots[i].positionY > height
+        || this.shots[i].positionY < 0) {
+        this.shots.splice(i, 1);
+      }
+    }
+  }
+}
