44bdc7ebe9e61f01202d17003eece8f63bac939b
Nicolai20HTXR <skole.nicolai.wiese@gmail.com>
2023-03-29 19:50:27 +0200

    removed uneeded function

--- src/quadtree.ts
+++ src/quadtree.ts
@@ -1,30 +1,7 @@
 import { Enemy } from './enemyClass';
 import { GatlingProjectile } from './gatlingProjectile';
 
-export function collideRectCircle(rx: number, ry: number, rw: number, rh: number, cx: number, cy: number, diameter: number) {
-	// 2d
-	// temporary variables to set edges for testing
-	let testX = cx;
-	let testY = cy;
 
-	// which edge is closest?
-	if (cx < rx) {
-		testX = rx; // left edge
-	} else if (cx > rx + rw) { testX = rx + rw; } // right edge
-
-	if (cy < ry) {
-		testY = ry; // top edge
-	} else if (cy > ry + rh) { testY = ry + rh; } // bottom edge
-
-	// // get distance from closest edges
-	const distance = dist(cx, cy, testX, testY);
-
-	// if the distance is less than the radius, collision!
-	if (distance <= diameter / 2) {
-		return true;
-	}
-	return false;
-}
 
 export class Quadtree {
 	public objects: Enemy[] = [];
